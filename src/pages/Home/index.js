d61fafea0f3c05fa1b0ef8290cb1a20884738dee
felizardo27 <jp.felizardo27@gmail.com>
2023-03-20 05:15:47 -0300

    feat: Creating title component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,17 @@
 import Banner from "components/Banner"
+import Footer from "components/Footer"
+import Header from "components/Header"
+import Title from "components/Title"
 
 const Home = () => {
   return (
     <>
+      <Header />
       <Banner imagem='home' />
+      <Title>
+        <h1>Um lugar para guardar seus vídeos e filmes!</h1>
+      </Title>
+      <Footer />
     </>
   )
 }
