0784521b670b31a2e81664af8ef8f01c73cfd692
felizardo27 <jp.felizardo27@gmail.com>
2023-03-20 20:35:21 -0300

    feat: Adding components to the favorites page

--- src/pages/Favorites/index.js
+++ src/pages/Favorites/index.js
@@ -1,9 +1,22 @@
+import Banner from 'components/Banner'
 import styles from './Favorites.module.css'
+import Title from 'components/Title'
+import Card from 'components/Card'
 
 const Favorites = () => {
   return (
     <>
-
+      <Banner imagem={'favorites'}/>
+      <Title>
+        <h1>Meus Favoritos</h1>
+      </Title>
+      <section className={styles.container}>
+      <Card 
+        id='2' 
+        title='Gato bonifácio' 
+        cover={'https://thecatapi.com/api/images/get?format=src&type=png'}
+      />           
+      </section>
     </>
   )
 }
