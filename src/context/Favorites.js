081b423cf98138a863c1d929678f08dca88503bb
felizardo27 <jp.felizardo27@gmail.com>
2023-03-21 05:45:57 -0300

    feat: Creating a custom favorite hook

--- src/context/Favorites.js
+++ src/context/Favorites.js
@@ -1,15 +1,40 @@
-import { createContext, useState } from "react";
+import { createContext, useContext, useState } from "react";
 
 export const FavoritesContext = createContext()
 FavoritesContext.displayName = "Favorites"
 
 export default function FavoritesProvider({ children }) {
 
-    const [Favorites, setFavorites] = useState()
+    const [favorites, setFavorites] = useState()
 
     return (
-        <FavoritesContext.Provider value={{ Favorites, setFavorites }}>
+        <FavoritesContext.Provider value={{ favorites, setFavorites }}>
             { children }
         </FavoritesContext.Provider>
     )
 } 
+
+export function useFavoritesContext() {
+
+    const [favorites, setFavorites] = useContext(FavoritesContext)
+
+    const addFavorites = (newFavorite) => {
+
+        const repeatedFavorite = favorites.some(favorite => favorite.id === newFavorite.id)
+
+        let newList = [...favorites]
+
+        if (!repeatedFavorite) {
+            newList.push(newFavorite)
+            return setFavorites(newList)
+        }
+
+        newList = favorites.filter(favorite => favorite.id !== newFavorite.id)
+        return setFavorites(newList)
+    }
+
+    return {
+        favorites,
+        addFavorites
+    }
+}
